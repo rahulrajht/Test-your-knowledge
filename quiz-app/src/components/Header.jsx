66869d340b83038a7b3d6ff383c8cc3bae6eb7d7
Rahul <rahulrajnkeht@gmail.com>
2022-01-17 17:59:33 +0530

    update score

--- quiz-app/src/components/Header.jsx
+++ quiz-app/src/components/Header.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect , useState} from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import '../styles/header.css'
+import "../styles/header.css";
+import { useSelector } from "react-redux";
+import Countdown from "react-countdown";
 function Header() {
-    return (
-        <Navbar className='navbar py-4' collapseOnSelect expand="lg" >
-        <Container>
-          <Navbar.Brand  className='text-white fw-bold'>Test Your Knowledge</Navbar.Brand>
-          <Nav >
-            <Nav.Link className='text-white'>Score : {10} points</Nav.Link>
-            <Nav.Link className='text-white'> Time Left: {20} Min </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    )
+  const quizDetails = useSelector((state) => state.quizReducer);
+
+  const {score} = useSelector((state) => state.scoreReducer);
+
+  return (
+    <Navbar className="navbar py-4" collapseOnSelect expand="lg">
+      <Container>
+        <Navbar.Brand className="text-white fw-bold">
+          Test Your Knowledge
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link className="text-white">Score : {score} points</Nav.Link>
+          <Nav.Link className="text-white">Time Left 00 : 00  </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
