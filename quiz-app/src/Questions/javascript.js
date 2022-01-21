562dab29d5d13b377d49046784647e6c53a342c7
Rahul <rahulrajnkeht@gmail.com>
2022-01-21 19:07:57 +0530

    added js questions

---
+++ quiz-app/src/Questions/javascript.js
@@ -0,0 +1,160 @@
+export const Easy_JS_Questions = [
+    {
+        q:" Which of the following is correct about JavaScript?",
+        options:["JavaScript is an Object-Based language","JavaScript is Assembly-language","JavaScript is an Object-Oriented language","JavaScript is a High-level language"],
+        ans:1,
+    },
+    {
+        q:" Arrays in JavaScript are defined by which of the following statements?",
+        options:["It is an ordered list of values","It is an ordered list of objects","It is an ordered list of string","It is an ordered list of functions"],
+        ans:1
+    },
+    {
+        q:"Which of the following is not javascript data types?",
+        options:["Null type","Undefined type","Number type"," All of the mentioned"],
+        ans:4
+    },
+    {
+        q:"Where is Client-side JavaScript code is embedded within HTML documents?",
+        options:["A URL that uses the special javascript:code","A URL that uses the special javascript:protocol","A URL that uses the special javascript:encoding","A URL that uses the special javascript:stack"],
+        ans:2
+    },
+    {
+        q:"Which of the following can be used to call a JavaScript Code Snippet?",
+        options:["Function/Method","Preprocessor","Triggering Event","RMI"],
+        ans:1
+    },
+    
+]
+
+export const MEDIUM_JS_QUESTIONS = [
+    {
+        q:"Why event handlers is needed in JS?",
+        options:["Allows JavaScript code to alter the behaviour of windows","Adds innerHTML page to the code","Change the server location","Performs handling of exceptions and occurrences"],
+        ans:1
+    },
+    {
+        q:" A JavaScript program can traverse and manipulate document content through which of the following object?",
+        options:["Both Element and Document Object","Data object","Element Object","Document Object"],
+        ans:1
+    },
+    {
+        q:"Which of the following is the property that is triggered in response to JS errors?",
+        options:["onclick","onerror","onmessage","onexception"],
+        ans:2
+    },
+    {
+        q:"Which of the following is not an error in JavaScript?",
+        options:["Missing of Bracket","Division by zero","Syntax error","Missing of semicolons"],
+        ans:2
+    },
+    {
+        q:"The main purpose of a “Live Wire” in NetScape is to ________",
+        options:["Create linkage between client side and server side"," Permit server side, JavaScript code, to connect to RDBMS","Support only non relational database","To interpret JavaScript code"],
+        ans:2
+    },
+    {
+        q:"The script tag must be placed in __________",
+        options:["the head tag","the head or body","the title or head","after the body tag"],
+        ans:2
+    },
+    {
+        q:" Which of the following scoping type does JavaScript use?",
+        options:["Sequential","Segmental","Lexical","Literal"],
+        ans:3
+    },
+    {
+        q:"What is the basic difference between JavaScript and Java?",
+        options:["Functions are considered as fields","Functions are values, and there is no hard distinction between methods and fields","Variables are specific","There is no difference"],
+        ans:2
+    },
+    {
+        q:"Why JavaScript Engine is needed?",
+        options:["Both Compiling & Interpreting the JavaScript","Parsing the javascript","Interpreting the JavaScript","Compiling the JavaScript"],
+        ans:3
+    },
+    {
+        q:"Which of the following is a fast C++ based JavaScript interpreter?",
+        options:["Closures","Processors","Node","Sockets"],
+        ans:3
+    },
+    
+]
+
+export const ADVANCE_JS_QUESTIONS =[
+    {
+        q:"Which of the following methods/operation does javascript use instead of == and !=?",
+        options:["JavaScript uses equalto()","JavaScript uses equals() and notequals() instead","JavaScript uses bitwise checking"," JavaScript uses === and !== instead"],
+        ans:4
+    },
+    {
+        q:"A JavaScript program developed on a Unix Machine ________",
+        options:["will throw errors and exceptions","must be restricted to a Unix Machine only","will work perfectly well on a Windows Machine","will be displayed as a JavaScript text on the browser"],
+        ans:3
+    },
+    {
+        q:"JavaScript is ideal to ________",
+        options:["make computations in HTML simpler","minimize storage requirements on the web server","increase the download time for the client","increase the loading time of the website"],
+        ans:2
+    },
+    {
+        q:"Which attribute is used to specify that the script is executed when the page has finished parsing? (only for external scripts)",
+        options:["parse"," a sync","defer","type"],
+        ans:3
+    },
+    {
+        q:"What must be done in order to implement Lexical Scoping?",
+        options:["Get the object","Dereference the current scope chain","Reference the current scope chain","Return the value"],
+        ans:3
+    },
+    {
+        q:"What is closure?",
+        options:[" Function objects","Scope where function’s variables are resolved","Both Function objects and Scope where function’s variables are resolved","Function return value"],
+        ans:3
+    },
+    {
+        q:"Which of the following uses a lot of CPU cycles?",
+        options:[" GUI","Statically generated graphics","Dynamically generated graphics","Generic scoping"],
+        ans:3
+    },
+    {
+        q:" Which of the following scoping type does JavaScript use?",
+        options:["Sequential","Segmental","Lexical","Literal"],
+        ans:3
+    },
+    {
+        q:"What is the basic difference between JavaScript and Java?",
+        options:["Functions are considered as fields","Functions are values, and there is no hard distinction between methods and fields","Variables are specific","There is no difference"],
+        ans:2
+    },
+    {
+        q:"A JavaScript program developed on a Unix Machine ________",
+        options:["will throw errors and exceptions","must be restricted to a Unix Machine only","will work perfectly well on a Windows Machine","will be displayed as a JavaScript text on the browser"],
+        ans:3
+    },
+    {
+        q:' What is the fundamental rule of lexical scoping?',
+        options:["Functions are declared in the scope","Functions are executed using scope chain","Functions are declared outside the scope","Variables are declared within the function"],
+        ans:2
+    },
+    {
+        q:"What is the opposite approach to the lexical scoping?",
+        options:["Literal scoping","Static scoping","Dynamic scoping","Generic scoping"],
+        ans:3
+    },
+    {
+        q:"Which of the algorithmic languages is not lexical scoping standardized in?",
+        options:["Ada","Pascal","Modula2","Html"],
+        ans:4
+    },
+    {
+        q:"he pop() method of the array does which of the following task?",
+        options:["decrements the total length by 1","increments the total length by 1","prints the first element but no effect on the length","updates the element"],
+        ans:1
+    },
+    {
+        q:"What will happen if reverse() and join() methods are used simultaneously?",
+        options:["Reverses and stores in the same array","Reverses and concatenates the elements of the array","Reverses","Stores the elements of an array in normal order"],
+        ans:1
+    }
+]
