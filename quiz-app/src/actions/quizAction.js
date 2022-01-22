e83132301328a6728da38e3c72ffdb4aeee50e0b
Rahul <rahulrajnkeht@gmail.com>
2022-01-22 18:34:26 +0530

    fixed timer issue

--- quiz-app/src/actions/quizAction.js
+++ quiz-app/src/actions/quizAction.js
@@ -1,6 +1,5 @@
 const CHOOSE_LANGUAGE = 'CHOOSE_LANGUAGE'
 const CHOOSE_LEVEL = 'CHOOSE_LEVEL'
-const CHOOSE_NO_OF_QUESTIONS = 'CHOOSE_NO_OF_QUESTIONS'
 const INC_SCORE = 'INC_SCORE';
 export const saveLanguageDetails = (data) =>(dispatch) =>{
     dispatch({
@@ -16,14 +15,6 @@ export const saveLevelDetails = (data) => (dispatch) =>{
         payload:data,
     })
     localStorage.setItem('level' , JSON.stringify(data))
-
-}
-export const saveNoQusDetails = (data) => (dispatch) =>{
-    dispatch({
-        type:CHOOSE_NO_OF_QUESTIONS,
-        payload:data,
-    })
-    localStorage.setItem('NoOfQuestions' , JSON.stringify(data))
 
 }
 export const incrementScore = () => (dispatch,getState) =>{
--- quiz-app/src/components/HomePage.jsx
+++ quiz-app/src/components/HomePage.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { saveLevelDetails } from "../actions/quizAction";
 import quizicon from "../images/quizicon.png";
-import '../styles/homepage.css'
+import "../styles/homepage.css";
 function HomePage() {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(saveLevelDetails(0))
+  }, []);
   return (
     <Container>
-      <Row>
+      <Row className="containers">
         <Col className="mt-5">
           <Card.Img src={quizicon} />
         </Col>
-        <Col className="mt-3 pt-3 ms-5 d-flex flex-column">
+        <Col className="mt-3 pt-3 margin d-flex flex-column">
           <Card.Title className="fs-1 fw-bold">Are you ready ?</Card.Title>
           <Card.Text className="fs-5 fw-normal">
-          A quiz or poll at the end of a lesson is a good way to gain a quick overview of who needs help and where you could challenge pupils further.
+            A quiz or poll at the end of a lesson is a good way to gain a quick
+            overview of who needs help and where you could challenge pupils
+            further.
           </Card.Text>
           <Card.Text className="fs-5 fw-normal">
-          And a quiz is a good way for a pupil to identify and take ownership of their knowledge gaps. It’s also a great way to help them to plug the gaps to boost their confidence when it comes to formal testing.
+            And a quiz is a good way for a pupil to identify and take ownership
+            of their knowledge gaps. It’s also a great way to help them to plug
+            the gaps to boost their confidence when it comes to formal testing.
           </Card.Text>
           <Link to={"/step1"}>
-          <Button className="btn-color mt-3 align-self-center px-5">Start</Button>
+            <Button className="btn-color mt-3 align-self-center px-5">
+              Start
+            </Button>
           </Link>
         </Col>
       </Row>
