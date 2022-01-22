bde65b44c85676658550e27ae1e10676b468c3d6
Rahul <rahulrajnkeht@gmail.com>
2022-01-22 18:34:58 +0530

    added html questions

---
+++ quiz-app/src/Questions/htmlQuestion.js
@@ -0,0 +1,158 @@
+export const EASY_HTML_QUESTIONS = [
+    {
+        q:" HTML stands for -",
+        options:["HighText Machine Language","HyperText and links Markup Language","HyperText Markup Language","None of these"],
+        ans:3
+    },
+    {
+        q:"The correct sequence of HTML tags for starting a webpage is -",
+        options:["Head, Title, HTML, body","HTML, Body, Title, Head","HTML, Head, Title, Body","HTML, Head, Title, Body"],
+        ans:4
+    },
+    {
+        q:"Which of the following element is responsible for making the text bold in HTML?",
+        options:["<pre>","<a>","<b>","<br>"],
+        ans:3
+    },
+    {
+        q:"How to create an unordered list (a list with the list items in bullets) in HTML?",
+        options:["<ul>","<ol>","<li>","<i>"],
+        ans:1
+    },
+    {
+        q:"Which character is used to represent the closing of a tag in HTML?",
+        options:["\\","!","/","."],
+        ans:3
+    },
+  
+]
+export const MEDIUM_HTML_QUESTIONS=[
+    {
+        q:"What are the types of unordered or bulleted list in HTML?",
+        options:["disc, square, triangle","polygon, triangle, circle","disc, circle, square","All of the above"],
+        ans:3
+    },
+    {
+        q:" Which of the following HTML attribute is used to define inline styles?",
+        options:["style","type","class","None of the above"],
+        ans:1
+    },
+    {
+        q:"A program in HTML can be rendered and read by -",
+        options:["Web browser","Server","Interpreter","None of the above"],
+        ans:1
+    },
+    {
+        q:"The tags in HTML are -",
+        options:["case-sensitive","in upper case","not case sensitive","in lowercase"],
+        ans:3
+    },
+    {
+        q:"Which of the following is not a difference between HTML and XHTML?",
+        options:["Charset in both html and xhtml is “text/html”","Tags and attributes are case-insensitive in HTML but not in XHTML","Special characters must be escaped using character entities in XHTML unlike HTML","Charset in html is “text/html” where as in xhtml it is “application/xml+xhtml”"],
+        ans:1
+    },
+    {
+        q:"In which part of the HTML metadata is contained?",
+        options:["head tag","title tag","html tag","body tag"],
+        ans:1
+    },
+    {
+        q:"Which of the following is not a HTML5 tag?",
+        options:["<track>"," <video>"," <slider>","<source>"],
+        ans:3
+    },
+    {
+        q:"404 HTTP error is generated due to ____________",
+        options:["on removing the newline character","missing JavaScript file","any missing file","slow loading of the webpage"],
+        ans:3
+    },
+    {
+        q:"Which of the following is not the element associated with the HTML table layout?",
+        options:["alignment","color","size","spanning"],
+        ans:2
+    },
+    {
+        q:"Which element is used for or styling HTML5 layout?",
+        options:["CSS","jQuery","JavaScript","PHP"],
+        ans:1
+    },
+    
+]
+export const ADVANCE_HTML_QUESTIONS =[
+    {
+        q:"Which HTML tag is used for making character appearance bold?",
+        options:["<u>content</u>","<b>content</b>","<br>content</br>","<i>content</i>"],
+        ans:2
+    },
+    {
+        q:"Which HTML element is used for short quote?",
+        options:["<em>","<abbr>","<q>","<blockquote>"],
+        ans:3
+    },
+    {
+        q:"Which HTML element is used for abbreviation or acronym?",
+        options:["<abbr>","<blockquote>","<q>","<em>"],
+        ans:1
+    },
+    {
+        q:"What is the work of <address> element in HTML5?",
+        options:["contains IP address","contains home address","contains url","contains contact details for author"],
+        ans:4
+    },
+    {
+        q:"What is the correct syntax of web address?",
+        options:["port://domain.filenmae:path/scheme/prefix","prefix://scheme.port:domain/filename/path","path://prefix.port:domain/filename/scheme","scheme://prefix.domain:port/path/filename"],
+        ans:4
+    },
+    {
+        q:"What is the use of <hr/> tag in HTML?",
+        options:["For making content appearance italics","To create vertical rule between sections","To create a line break","To create horizontal rule between sections"],
+        ans:4
+    },
+    {
+        q:"Which attribute is not essential under <iframe>?",
+        options:["frameborder","width","height","src"],
+        ans:1
+    },
+    {
+        q:"Which of the following is an HTML specification used to add more information to HTML tags?",
+        options:["Modifydata","Minidata","Macrodata","Microdata"],
+        ans:4
+    },
+    {
+        q:"Which HTML element is used for YouTube videos?",
+        options:["<samp>","<small>","<frame>","<iframe>"],
+        ans:4
+    },
+    {
+        q:"Which of the following HTML element is used for canvas graphics?",
+        options:["<css>","<paint>","<canvas>","<graphic>"],
+        ans:3
+    },
+    {
+        q:"Which element is known as HTML plaintext element?",
+        options:["<spacer>","<spacer>","<shadow>","<plaintext>"],
+        ans:4
+    },
+    {
+        q:"__________ is the XML equivalent to strict HTML 4.01.",
+        options:["XHTML 1.0 Strict","XHTML 1.0 Frameset","XHTML 1.0 Transitional","XHTML 1.1 Strict"],
+        ans:1
+    },
+    {
+        q:"Which of the following statement is true?",
+        options:["Attribute names must be in uppercase","Attribute values must be quoted","Attribute minimization is mandatory","Attribute values should not be quoted"],
+        ans:2
+    },
+    {
+        q:"Which of the following encoding an XML parser assumes?",
+        options:["UTF-8","UTF-16","UTF-32","Both UTF-8 and UTF-32"],
+        ans:4
+    },
+    {
+        q:"__________ is an extended version of the XHTML markup language for supporting RDF through a collection of attributes and processing rules in the form of well-formed XML documents.",
+        options:["XHTML + RDFa","XHTML+XHTML5","XHTML1.0+XHTML2.0","XHTML + RDF"],
+        ans:1
+    }
+]
--- quiz-app/src/screens/Question.jsx
+++ quiz-app/src/screens/Question.jsx
@@ -4,29 +4,40 @@ import FormContainer from "../components/FormContainer";
 import "../styles/questions.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
-import { ADVANCE_JS_QUESTIONS, Easy_JS_Questions, MEDIUM_JS_QUESTIONS } from "../Questions/javascript";
+import {
+  ADVANCE_JS_QUESTIONS,
+  Easy_JS_Questions,
+  MEDIUM_JS_QUESTIONS,
+} from "../Questions/javascript";
 import { useEffect } from "react";
 import { incrementScore } from "../actions/quizAction";
 import { useNavigate } from "react-router-dom";
-import { ADVANCE_REACT_QUESTIONS, EASY_REACT_QUESTIONS, MEDIUM_REACT_QUESTIONS } from "../Questions/reactQuestion";
-
+import {
+  ADVANCE_REACT_QUESTIONS,
+  EASY_REACT_QUESTIONS,
+  MEDIUM_REACT_QUESTIONS,
+} from "../Questions/reactQuestion";
+import { EASY_HTML_QUESTIONS,MEDIUM_HTML_QUESTIONS,ADVANCE_HTML_QUESTIONS } from "../Questions/htmlQuestion";
 function Question() {
   const sample = [
     {
       q: "Sample Question",
       options: ["A", "B", "C", "D"],
-      ans: 1,
+      ans: 5,
     },
   ];
   const [questions, setquestions] = useState(sample);
   const quizReducer = useSelector((state) => state.quizReducer);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { language,  level } = quizReducer;
+  const { language, level } = quizReducer;
 
   let [count, setCount] = useState(0);
 
   function nextQuestion(i) {
+    if (questions[0].q === sample[0].q) {
+      return;
+    }
     if (questions.length - 1 === count) {
       if (questions[count].ans === i + 1) {
         dispatch(incrementScore());
@@ -39,28 +50,34 @@ function Question() {
   }
 
   useEffect(() => {
-    if (language === "JS"  && level === 1) {
+    if (language === "JS" && level === 1) {
       setquestions(Easy_JS_Questions);
     }
-    if(language === "JS"  && level === 2){
-      setquestions(MEDIUM_JS_QUESTIONS)
+    if (language === "JS" && level === 2) {
+      setquestions(MEDIUM_JS_QUESTIONS);
     }
-    if(language === "JS"  && level === 3){
-      setquestions(ADVANCE_JS_QUESTIONS)
+    if (language === "JS" && level === 3) {
+      setquestions(ADVANCE_JS_QUESTIONS);
     }
-    if(language === 'REACT' && level === 1){
-      setquestions(EASY_REACT_QUESTIONS)
+    if (language === "REACT" && level === 1) {
+      setquestions(EASY_REACT_QUESTIONS);
     }
-    if(language === 'REACT' && level === 2){
-      setquestions(MEDIUM_REACT_QUESTIONS)
+    if (language === "REACT" && level === 2) {
+      setquestions(MEDIUM_REACT_QUESTIONS);
     }
-    if(language === 'REACT' && level === 3){
-      setquestions(ADVANCE_REACT_QUESTIONS)
+    if (language === "REACT" && level === 3) {
+      setquestions(ADVANCE_REACT_QUESTIONS);
     }
-    
+    if(language === "HTML" && level === 1 ){
+      setquestions(EASY_HTML_QUESTIONS)
+    }    
+    if(language === "HTML" && level === 2 ){
+      setquestions(MEDIUM_HTML_QUESTIONS)
+    }    
+    if(language === "HTML" && level === 3 ){
+      setquestions(ADVANCE_HTML_QUESTIONS)
+    }
   }, [language, level]);
-
-  
 
   return (
     <FormContainer>
@@ -71,6 +88,7 @@ function Question() {
           </Col>
         </Row>
       </Container>
+
       <Container className="mt-5 text-center">
         <Row className="mt-5">
           <Col onClick={() => nextQuestion(0)} className="options">
@@ -80,6 +98,7 @@ function Question() {
             {questions[count].options[1]}
           </Col>
         </Row>
+
         <Row className="mt-5">
           <Col onClick={() => nextQuestion(2)} className="options">
             {questions[count].options[2]}
