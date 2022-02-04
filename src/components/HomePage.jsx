4c7ef4b2768f506086950ad6fc614f5c70dd7f86
Rahul <rahulrajnkeht@gmail.com>
2022-02-04 15:13:23 +0530

    added missing dependency

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(saveLevelDetails(0))
-  }, []);
+  }, [dispatch]);
   return (
     <Container>
       <Row className="containers">
